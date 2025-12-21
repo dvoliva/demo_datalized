fb1a5432f4d0a312b3f56c0eb21746b318a6927d
dvoliva <venegasolivad@gmail.com>
2025-12-21 01:10:26 -0300

    implement extractions and transformation logic

--- src/etl.py
+++ src/etl.py
@@ -15,25 +15,46 @@ def load_data():
                 sep=';',
                 usecols=config.REQUIRED_COLUMNS,
                 encoding='utf-8',
-                dtype={'mrun': str, 'cat_periodo': int}
+                dtype={'mrun': str}
             )
 
-            filtered_df = current_df[current_df['cat_periodo'] >= config.MIN_YEAR]
-
-            if not filtered_df.empty:
-                data_frames.append(filtered_df)
-                print('archivo cargado: ' + os.path.basename(file_path))
+            data_frames.append(current_df)
+            print(f'archivo: {os.path.basename(file_path)} | filas: {len(data_frames)}')
+        
         except Exception as e:
-            print('error en la carga ' + file_path + ': ' + str(e))
+            print(f'error en {os.path.basename(file_path)}: {e}')
     
     if not data_frames:
         return pd.DataFrame()
-
+    
     return pd.concat(data_frames, ignore_index=True)
+
+def transform_data(df_input):
+    df = df_input.copy()
+
+    df = df[~df['anio_ing_carr_act'].isin(config.INVALID_ADMISSION_YEARS)]
+
+    text_cols = ['tipo_inst_1', 'modalidad', 'jornada', 'rango_edad']
+    for col in text_cols:
+        if col in df.columns:
+            df[col] = df[col].astype(str).str.title().str.strip()
+
+    is_not_presential = ~df['modalidad'].str.contains('Presencial', case=False, na=False)
+    is_vesp = df['jornada'].str.contains('Vespertino|A Distancia', case=False, na=False)
+
+    df['segmento'] = 'Tradicional'
+
+    df.loc[is_not_presential | is_vesp, 'segmento'] = 'Flexible'
+
+    df['gen_alu'] = df['gen_alu'].map(config.GENDER_MAP)
+
+    return df
+
 
 if __name__ == "__main__":
     df_test = load_data()
-    
+    df_mrun = df_test["mrun"].nunique()
+    print(f'Unique cat_periodo values: {df_mrun}')
     if not df_test.empty:
         print(df_test.head())
     else:
